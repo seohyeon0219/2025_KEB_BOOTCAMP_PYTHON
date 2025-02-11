455f92073d69eafb2a1bc856c8430b9cf233720c
Kimseohyeon <seohyeon0219@naver.com>
2025-02-11 13:31:40 +0900

    v1.0 day02 prime number

--- day02.py
+++ day02.py
@@ -10,23 +10,32 @@ print(type(2**10)) #1024
 print(type(16**0.5)) #4.0
 #반복문에서 float는 x이므로 형 변환해야 함
 
+def is_prime(num) -> bool: #is_~ -> true or false
+    """
+    소수 여부를 판정해서 소수면 true, 소수가 아니면 false를 리턴하는 함수
+    A function that returns True if it is a prime number and False if it is not a prime number
+    :param num: integer number
+    :return: boolean type
+    """
+    if num >= 2:
+        for i in range(2, int(num ** 0.5)+1):
+            if num % i == 0:
+                return False
+                #is_prime = False
+                #break
+            #print(i, end=' ')
+
+    else:
+        return False
+    return True
+
+#main
+help(abs) #abs함수에 대한 설명
+help(is_prime) #내가 만든 is_prime 함수에 대한 설명
 n = int(input("Input number : "))
 
-is_prime = True
-
-if n >= 2:
-    # for i in range(2, n):
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            # count = count + 1
-            is_prime = False
-            break
-        print(i, end = ' ')
-else:
-    if_prime = False
-
 # if count == 0:
-if is_prime:
+if is_prime(n): #function call
     print(f"{n} is prime number")
 else :
     print(f"{n} is not prime number")
