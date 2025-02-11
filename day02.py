79f91d99f14359861e943e671d201e739d860950
Kimseohyeon <seohyeon0219@naver.com>
2025-02-11 11:39:43 +0900

    v0.9 day02 prime number

--- day02.py
+++ day02.py
@@ -6,12 +6,17 @@
 #    for i in range(1, 10):
 #        print(f"{dan} * {i} = {dan*i}")
 
+print(type(2**10)) #1024
+print(type(16**0.5)) #4.0
+#반복문에서 float는 x이므로 형 변환해야 함
+
 n = int(input("Input number : "))
 
 is_prime = True
 
 if n >= 2:
-    for i in range(2, n):
+    # for i in range(2, n):
+    for i in range(2, int(n**0.5)+1):
         if n % i == 0:
             # count = count + 1
             is_prime = False
