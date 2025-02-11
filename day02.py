af320ed2e02d972117b2ac0cfa7307de8b9b30ab
Kimseohyeon <seohyeon0219@naver.com>
2025-02-12 00:35:23 +0900

    v0.1

--- day02.py
+++ day02.py
@@ -1,5 +1,4 @@
-# ** 대신 pow 함수
-import math
+# 구간 소수 찾기 프로그램
 
 def is_prime(num) -> bool:
     """
@@ -9,7 +8,7 @@ def is_prime(num) -> bool:
     """
     if num >= 2:
         i = 2
-        while(i <= int(math.pow(num,0.5)) + 1 ):
+        while(i <= int(num**0.5) + 1 ):
             if num % i == 0:
                 return False
             i += 1
