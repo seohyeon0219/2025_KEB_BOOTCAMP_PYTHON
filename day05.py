f8dbe18e042587eecb3e035fbf85ddad7fd38218
Kimseohyeon <seohyeon0219@naver.com>
2025-02-14 11:19:56 +0900

    v4.1 day05 countdown

--- day05.py
+++ day05.py
@@ -1,30 +1,35 @@
+#1
+def bomb_repetition(n):
+    for i in range(n, 0, -1):
+        print(i)
+    print("펑")
 
-def fibonacci_recursion(n) -> int:
-    """
-    피보나치 수 계산함수 (재귀함수 버전)
-    :param n:
-    :return: 피보나치 계산 결과 값
-    """
-    if n <= 0:
-        return 0
-    elif n == 1:
-        return 1
+def bomb_recursion(n):
+    if n == 0:
+        print("펑")
     else:
-        return fibonacci_recursion(n-2) + fibonacci_recursion(n-1)
+        print(n)
+        bomb_recursion(n-1)
+
+#2 교수님 코드
+def countdown_loop(n):
+    for i in range(n, -1, -1):
+        if i == 0:
+            print("펑~")
+        else:
+            print(i)
 
 
-def fibonacci_loop(n) -> int:
-    """
-    피보나치 수 계산함수 (반복문 버전)
-    :param n:
-    :return: 피보나치 계산 결과 값
-    """
-    n_list=[0 ,1]
-    for i in range(n+1):
-        n_list.append(n_list[i] + n_list[i + 1])
+def countdown_recursion(n):
+    if n < 0:
+        return
+    if n == 0:
+        print("펑!")
+    else:
+        print(n)
+    countdown_recursion(n-1)
 
-    return n_list[n]
 
 n = int(input())
-print(fibonacci_loop(n))
-print(fibonacci_recursion(n))
+#countdown_loop(n)
+countdown_recursion(n)
